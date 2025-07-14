750608316f0c138eec4db7a7c42eec51817f51c4
TurtleGITHUB <76065011+TurtleGITHUB@users.noreply.github.com>
2025-07-14 00:55:18 +0000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -16,6 +16,17 @@ module.exports = function(eleventyConfig) {
       day: 'numeric'
     });
   });
+
+  // Add the "head" filter for slicing arrays
+  eleventyConfig.addFilter("head", function(array, n) {
+    if (!Array.isArray(array) || array.length === 0) {
+      return [];
+    }
+    if(n < 0) {
+      return array.slice(n);
+    }
+    return array.slice(0, n);
+  });
   
   // Add a collection for posts
   eleventyConfig.addCollection("posts", function(collectionApi) {
